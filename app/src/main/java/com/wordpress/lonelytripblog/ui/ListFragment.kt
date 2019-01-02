ba358a8696a9cb4394b4936ba3b231de3fc46a65
pavel <pavelsuvit@gmail.com>
2019-01-03 00:34:02 +0300

    Add nice look for ListFragment (with the help of CardView)

--- app/src/main/java/com/wordpress/lonelytripblog/ui/ListFragment.kt
+++ app/src/main/java/com/wordpress/lonelytripblog/ui/ListFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.wordpress.lonelytripblog.R
 import com.wordpress.lonelytripblog.data.Recipe
 import com.wordpress.lonelytripblog.viewmodel.Food2ForkViewModel
